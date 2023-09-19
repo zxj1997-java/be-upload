aae78da07ec0a15074dad3e64fcb5f952cfc4b08
zhangxingju <1533195362@qq.com>
2023-09-19 13:42:40 +0800

    refactor(基于layui的上传插件):实现点击图片放大效果

--- plugins/BeUpload.js
+++ plugins/BeUpload.js
@@ -1,6 +1,8 @@
 /** BeUpload.js **/
 layui.define(['jquery', 'layer', 'form'], function (exports) {
     let $ = layui.jquery;
+    let layer = layui.layer;
+
     let render = function (option) {
         $(option.id).append(`
             <div class="upload-wapper">
@@ -20,25 +22,27 @@ layui.define(['jquery', 'layer', 'form'], function (exports) {
             // 可以将文件对象传递给其他函数进行进一步处理
             processFile(option, file);
         });
+        // 清除已选中的文件
+        $(this).val('');
     }
 
 
     // 在此处执行对文件的自定义处理逻辑
     let processFile = function (option, file) {
         console.log(file)
-        if (option.size >= file.size / 1024 / 1024) {
-            $(option.id).prepend(`
+        if (file) {
+            if (option.size && option.size <= file.size / 1024 / 1024) {
+                layer.msg("上传文件大小不得超过" + option.size + "M", {icon: 0});
+            } else {
+                $(option.id).prepend(`
             <div class="image-section" data-id="1111">
-                <img class="image-show" src="https://picx.zhimg.com/70/v2-d094166746ca67d2a2ed61042e2675ce_1440w.avis?source=172ae18b&biz_tag=Post">
+                <img class="image-show" src="https://pic-zxj.oss-cn-shanghai.aliyuncs.com/20230710110151.png">
                 <div class="image-shade"></div>
                 <i class="delete-icon"></i>
                 <i class="zoom-icon"></i>
             </div>`);
-
-            showAddBtn(option);
-        }
-        else{
-            console.log("文件太大")
+                showAddBtn(option);
+            }
         }
     }
 
@@ -53,14 +57,22 @@ layui.define(['jquery', 'layer', 'form'], function (exports) {
 
         // 在此处执行点击放大图标后的逻辑
         $(option.id).on('click', '.zoom-icon', function () {
-            console.log("放大图标");
+            let img = $(this).parent(".image-section").children("img")[0];
+            layer.open({
+                type: 1,
+                area: "auto", // 宽高
+                title: false, // 不显示标题栏
+                closeBtn: 0,
+                shadeClose: true, // 点击遮罩关闭层
+                content: "<img src='"+img.src+"' style='max-width: 900px;height: auto'/>"
+            });
         });
     }
 
     //显示或隐藏添加按钮
     function showAddBtn(option) {
         let count = $(option.id + ' .image-section').length;
-        if (count >= option.num) {
+        if (option.num && count >= option.num) {
             $(option.id).children().last().hide();
         } else {
             $(option.id).children().last().show();
