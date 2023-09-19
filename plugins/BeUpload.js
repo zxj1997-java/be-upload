4063206fa4851cc09696ec7338bfc3dc47b98362
zhangxingju <1533195362@qq.com>
2023-09-19 12:10:20 +0800

    refactor(基于layui的上传插件):完成对文件大小 数量的判断

--- plugins/BeUpload.js
+++ plugins/BeUpload.js
@@ -9,6 +9,7 @@ layui.define(['jquery', 'layer', 'form'], function (exports) {
             </div>`)
 
         appendChange(option);
+        clickEvent(option);
     }
 
     let appendChange = function (option) {
@@ -17,15 +18,53 @@ layui.define(['jquery', 'layer', 'form'], function (exports) {
             let files = event.target.files;
             let file = files[0];
             // 可以将文件对象传递给其他函数进行进一步处理
-            processFile(file);
+            processFile(option, file);
         });
     }
 
 
     // 在此处执行对文件的自定义处理逻辑
-    function processFile(file) {
+    let processFile = function (option, file) {
         console.log(file)
+        if (option.size >= file.size / 1024 / 1024) {
+            $(option.id).prepend(`
+            <div class="image-section" data-id="1111">
+                <img class="image-show" src="https://picx.zhimg.com/70/v2-d094166746ca67d2a2ed61042e2675ce_1440w.avis?source=172ae18b&biz_tag=Post">
+                <div class="image-shade"></div>
+                <i class="delete-icon"></i>
+                <i class="zoom-icon"></i>
+            </div>`);
 
+            showAddBtn(option);
+        }
+        else{
+            console.log("文件太大")
+        }
+    }
+
+    //删除或者放大图片
+    function clickEvent(option) {
+        // 在此处执行点击删除图标后的逻辑
+        $(option.id).on('click', '.delete-icon', function () {
+            console.log("删除图标");
+            $(this).parent(".image-section").remove();
+            showAddBtn(option);
+        });
+
+        // 在此处执行点击放大图标后的逻辑
+        $(option.id).on('click', '.zoom-icon', function () {
+            console.log("放大图标");
+        });
+    }
+
+    //显示或隐藏添加按钮
+    function showAddBtn(option) {
+        let count = $(option.id + ' .image-section').length;
+        if (count >= option.num) {
+            $(option.id).children().last().hide();
+        } else {
+            $(option.id).children().last().show();
+        }
     }
 
     exports('BeUpload', {
