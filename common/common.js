b75cf32171b363e23c9389600d575a7bb85866c7
zhangxingju <1533195362@qq.com>
2023-09-19 14:33:11 +0800

    refactor(基于layui的上传插件):文件上传组件添加

--- common/common.js
+++ common/common.js
@@ -3,4 +3,5 @@ layui.config({
     base: '/plugins/' // 配置 Layui 第三方扩展模块存放的基础目录
 }).extend({
     BeImgUpload: 'BeImgUpload', // 定义模块名和模块文件路径，继承 layui.config 的 base 路径
+    BeFileUpload: 'BeFileUpload',
 });
---
+++ plugins/BeFileUpload.js
@@ -0,0 +1,125 @@
+/** BeImgUpload.js **/
+layui.define(['jquery', 'layer', 'form'], function (exports) {
+    let $ = layui.jquery;
+    let layer = layui.layer;
+
+    let render = function (option) {
+        addImg(option);
+        $(option.id).append(`
+            <div class="upload-wapper">
+                <i class="upload-icon"></i>
+                <input type="file" name="${option.name}" class="upload-input" accept="${option.type ? option.type : 'image/*'}">
+            </div>`)
+        appendChange(option);
+        clickEvent(option);
+    }
+
+    let appendChange = function (option) {
+        $("input[name='" + option.name + "']").on('change', function (event) {
+            // 获取选择的文件列表
+            let files = event.target.files;
+            let file = files[0];
+            // 可以将文件对象传递给其他函数进行进一步处理
+            processFile(option, file);
+        });
+        // 清除已选中的文件
+        $(this).val('');
+    }
+
+
+    // 在此处执行对文件的自定义处理逻辑
+    let processFile = function (option, file) {
+        console.log(file)
+        if (file) {
+            if (option.size && option.size <= file.size / 1024 / 1024) {
+                layer.msg("上传文件大小不得超过" + option.size + "M", {icon: 0});
+            } else {
+                let that = $(this);
+                let formData = new FormData();
+                formData.append("file", file);
+                debugger
+                // $.ajax({
+                //     url: "http://localhost:8080/sys/file/add",
+                //     data: formData,
+                //     success: function (result) {
+                $(option.id).prepend(`
+                <div class="image-section" data-id="1111">
+                    <img class="image-show" src="https://pic-zxj.oss-cn-shanghai.aliyuncs.com/20230626150654.png">
+                    <div class="image-shade"></div>
+                    <i class="delete-icon"></i>
+                    <i class="zoom-icon"></i>
+                </div>`);
+                showAddBtn(option);
+                //     },
+                //     error: function (xhr, status, error) {
+                //         layer.msg("删除失败", {icon: 2})
+                //     }
+                // });
+
+
+            }
+        }
+    }
+
+    //添加预览图片
+    let addImg = function (option) {
+        if (option.value) {
+            for (let i = 0; i < option.value.length; i++) {
+                $(option.id).append(`
+                <div class="image-section" data-id="1111">
+                    <img class="image-show" src="https://pic-zxj.oss-cn-shanghai.aliyuncs.com/${option.value[i]}">
+                    <div class="image-shade"></div>
+                    <i class="delete-icon"></i>
+                    <i class="zoom-icon"></i>
+                </div>`);
+            }
+            showAddBtn(option);
+        }
+    }
+
+    //删除或者放大图片
+    function clickEvent(option) {
+        // 在此处执行点击删除图标后的逻辑
+        $(option.id).on('click', '.delete-icon', function () {
+            let that = $(this);
+            // $.ajax({
+            //     url: "http://localhost:8080/sys/file/deleteId",
+            //     data: {id: that.data("id")},
+            //     success: function (result) {
+            that.parent(".image-section").remove();
+            showAddBtn(option);
+            //     },
+            //     error: function (xhr, status, error) {
+            //         layer.msg("删除失败", {icon: 2})
+            //     }
+            // });
+        });
+
+        // 在此处执行点击放大图标后的逻辑
+        $(option.id).on('click', '.zoom-icon', function () {
+            let img = $(this).parent(".image-section").children("img")[0];
+            layer.open({
+                type: 1,
+                area: "auto", // 宽高
+                title: false, // 不显示标题栏
+                closeBtn: 1,
+                shadeClose: true, // 点击遮罩关闭层
+                content: "<img src='" + img.src + "' style='max-width: 900px;height: auto'/>"
+            });
+        });
+    }
+
+    //显示或隐藏添加按钮
+    function showAddBtn(option) {
+        let count = $(option.id + ' .image-section').length;
+        if (option.num && count >= option.num) {
+            $(option.id).children().last().hide();
+        } else {
+            $(option.id).children().last().show();
+        }
+    }
+
+    exports('BeFileUpload', {
+        render
+    });
+});
