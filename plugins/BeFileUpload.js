5c3260d55d93b2c5e224ae15eb4b83ff85cb0cad
zhangxingju <1533195362@qq.com>
2023-11-08 16:10:58 +0800

    style(图片上传): 合理使用prototype 优化代码结构

--- plugins/BeFileUpload.js
+++ plugins/BeFileUpload.js
@@ -1,46 +1,46 @@
-/** BeImgUpload.js **/
+/** BeFileUpload.js **/
 layui.define(['jquery', 'layer', 'form'], function (exports) {
     let $ = layui.jquery;
     let layer = layui.layer;
 
-    let render = function (option) {
-        $(option.id).css("display", "grid")
-
-        option.value = option.value ? option.value : [];
-        $(option.id).append(`<input type="hidden" class="upload-hidden-input" name="${option.name}"/>`)
-        $(option.id).append(`
-            <div class="doc-tips-group block">
-            <span class="doc-tips block" style=>≤10M,支持 doc, docx, xls, xsx
-            </span>
-            <div class="doc-uploadBtn">
-                <i style="font-style: normal;">上传</i>
-                <input class="upload-input" type="file" accept="${option.type ? option.type : 'image/*'}"/>
-            </div>
-        </div>`);
-        addImg(option);
-        appendChange(option);
-        clickEvent(option);
+    function BeFileUpload(option) {
+        this.option = option;
+        this.value = option.value ? option.value : [];
+        this.init();
     }
 
-    let appendChange = function (option) {
-        $(option.id + " input[type=file]").on('change', function (event) {
-            // 获取选择的文件列表
+    BeFileUpload.prototype.init = function () {
+        $(this.option.id).css("display", "grid");
+        $(this.option.id).append(`<input type="hidden" class="upload-hidden-input" name="${this.option.name}"/>`);
+        $(this.option.id).append(`
+            <div class="doc-tips-group block">
+                <span class="doc-tips block">≤10M,支持 doc, docx, xls, xsx</span>
+                <div class="doc-uploadBtn">
+                    <i style="font-style: normal;">上传</i>
+                    <input class="upload-input" type="file" accept="${this.option.type ? this.option.type : 'image/*'}"/>
+                </div>
+            </div>`);
+        this.addFiles();
+        this.appendChange();
+        this.clickEvent();
+    };
+
+    BeFileUpload.prototype.appendChange = function () {
+        let that = this;
+        $(this.option.id + " input[type=file]").on('change', function (event) {
             let files = event.target.files;
             let file = files[0];
-            // 可以将文件对象传递给其他函数进行进一步处理
-            processFile(option, file);
-            $(option.id + " input[type=file]").val(""); // 强制清除输入框的值
+            that.processFile(file);
+            $(that.option.id + " input[type=file]").val("");
         });
-        // 清除已选中的文件
         $(this).val('');
-    }
+    };
 
-
-    // 在此处执行对文件的自定义处理逻辑
-    let processFile = function (option, file) {
+    BeFileUpload.prototype.processFile = function (file) {
+        let that = this;
         if (file) {
-            if (option.size && option.size <= file.size / 1024 / 1024) {
-                layer.msg("上传文件大小不得超过" + option.size + "M", {icon: 0});
+            if (that.option.size && that.option.size <= file.size / 1024 / 1024) {
+                layer.msg("上传文件大小不得超过" + that.option.size + "M", {icon: 0});
             } else {
                 let formData = new FormData();
                 formData.append("file", file);
@@ -48,106 +48,103 @@ layui.define(['jquery', 'layer', 'form'], function (exports) {
                     url: ctx_ + "document/file/upload",
                     data: formData,
                     type: "post",
-                    processData: false,  // 禁止对数据进行序列化
-                    contentType: false,  // 不设置请求头的 Content-Type
+                    processData: false,
+                    contentType: false,
                     success: function (result) {
-                        $(option.id).append(`
-                        <div data-id="${result.id}" class="doc-item-bg">
-                            <img class="doc-img" 
-                            src="${getFileType(file)}">
-                            <div class="block doc-info-detail">
-                                <span class="doc-title">${result.fileName}</span> 
-                                <span class="doc-desc">${Math.round(result.fileSize / 1024)}KB</span>
-                            </div>
-                            <div class="block doc-icon-group">
-                                <img class="doc-icon download" src="./common/img/doc_download.png">
-                                <img class="doc-icon delete" src="./common/img/doc_delete.png">
-                            </div>
-                        </div>`);
-                        option.value.push(result.id);
-                        $(option.id + " input[name='" + option.name + "']").val(option.value.join(','))
-                        showAddBtn(option);
+                        $(that.option.id).append(`
+                            <div data-id="${result.id}" class="doc-item-bg">
+                                <img class="doc-img" src="${that.getFileType(file)}">
+                                <div class="block doc-info-detail">
+                                    <span class="doc-title">${result.fileName}</span> 
+                                    <span class="doc-desc">${Math.round(result.fileSize / 1024)}KB</span>
+                                </div>
+                                <div class="block doc-icon-group">
+                                    <img class="doc-icon download" src="./common/img/doc_download.png">
+                                    <img class="doc-icon delete" src="./common/img/doc_delete.png">
+                                </div>
+                            </div>`
+                        );
+                        that.value.push(result.id);
+                        $(that.option.id + " input[name='" + that.option.name + "']").val(that.value.join(','));
+                        that.showAddBtn();
                     },
                     error: function (xhr, status, error) {
-                        layer.msg("上传失败", {icon: 2})
+                        layer.msg("上传失败", {icon: 2});
                     }
                 });
             }
         }
-    }
+    };
 
-    //添加预览图片
-    let addImg = function (option) {
-        if (option.value) {
-            $(option.id + " input[name='" + option.name + "']").val(option.value.join(','))
-            showAddBtn(option);
+    BeFileUpload.prototype.addFiles = function () {
+        let that = this;
+        if (that.value) {
+            $(that.option.id + " input[name='" + that.option.name + "']").val(that.value.join(','));
+            that.showAddBtn();
             $.ajax({
                 url: ctx_ + "work/user/findFileById",
-                data: JSON.stringify(option.value),
+                data: JSON.stringify(that.value),
                 contentType: 'application/json;charset=UTF-8',
                 type: "post",
                 success: function (result) {
                     for (let i = 0; i < result.length; i++) {
-                        $(option.id).append(`
-                        <div data-id="${result[i].id}" class="doc-item-bg">
-                            <img class="doc-img" src="./common/img/icon-image.png">
-                            <div class="block doc-info-detail">
-                                <span class="doc-title">${result[i].fileName}</span> 
-                                <span class="doc-desc">${Math.round(result[i].fileSize / 1024)}KB</span>
-                            </div>
-                            <div class="block doc-icon-group">
-                                <img class="doc-icon download" src="./common/img/doc_download.png">
-                                <img class="doc-icon delete" src="./common/img/doc_delete.png">
-                            </div>
-                        </div>`);
+                        $(that.option.id).append(`
+                            <div data-id="${result[i].id}" class="doc-item-bg">
+                                <img class="doc-img" src="./common/img/icon-image.png">
+                                <div class="block doc-info-detail">
+                                    <span class="doc-title">${result[i].fileName}</span> 
+                                    <span class="doc-desc">${Math.round(result[i].fileSize / 1024)}KB</span>
+                                </div>
+                                <div class="block doc-icon-group">
+                                    <img class="doc-icon download" src="./common/img/doc_download.png">
+                                    <img class="doc-icon delete" src="./common/img/doc_delete.png">
+                                </div>
+                            </div>`
+                        );
                     }
                 },
                 error: function (xhr, status, error) {
-                    layer.msg("获取失败", {icon: 2})
+                    layer.msg("获取失败", {icon: 2});
                 }
             });
         }
-    }
+    };
 
-
-    //删除或者放大图片
-    function clickEvent(option) {
-        // 在此处执行点击删除图标后的逻辑
-        $(option.id).on('click', '.delete', function () {
+    BeFileUpload.prototype.clickEvent = function () {
+        let that = this;
+        // Execute logic when clicking on delete icon
+        $(this.option.id).on('click', '.delete', function () {
             let that = $(this);
             let id = that.parent().parent(".doc-item-bg").data("id");
             that.parent().parent(".doc-item-bg").remove();
-            showAddBtn(option);
-            let index = option.value.indexOf(id);
+            that.showAddBtn();
+            let index = that.value.indexOf(id);
             if (index !== -1) {
-                option.value.splice(index, 1);
+                that.value.splice(index, 1);
             }
-            $(option.id + " input[name='" + option.name + "']").val(option.value.join(','));
+            $(that.option.id + " input[name='" + that.option.name + "']").val(that.value.join(','));
         });
 
-        // 在此处执行点击放大图标后的逻辑
-        $(option.id).on('click', '.download', function () {
+        // 单击下载图标时执行逻辑
+        $(this.option.id).on('click', '.download', function () {
             let that = $(this);
             let id = that.parent().parent(".doc-item-bg").data("id");
-            window.open(ctx_ + "document/file/download?fileId=" + id)
+            window.open(ctx_ + "document/file/download?fileId=" + id);
         });
-    }
+    };
 
-    //显示或隐藏添加按钮
-    function showAddBtn(option) {
-        let count = $(option.id + ' .doc-item-bg').length;
-        if (option.num && count >= option.num) {
+    BeFileUpload.prototype.showAddBtn = function () {
+        let count = $(this.option.id + ' .doc-item-bg').length;
+        if (this.option.num && count >= this.option.num) {
             $(".doc-uploadBtn").hide();
         } else {
             $(".doc-uploadBtn").show();
         }
-    }
+    };
 
-    function getFileType(file) {
-        // 获取文件的扩展名
+    BeFileUpload.prototype.getFileType = function (file) {
         const extension = file.name.split('.').pop().toLowerCase();
 
-        // 定义常见文件类型和对应的扩展名列表
         const documentExtensions = ['doc', 'docx', 'odt'];
         const spreadsheetExtensions = ['xls', 'xlsx', 'ods'];
         const imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
@@ -170,10 +167,11 @@ layui.define(['jquery', 'layer', 'form'], function (exports) {
         } else {
             return './common/img/icon-file.png';
         }
-    }
-
+    };
 
     exports('BeFileUpload', {
-        render
+        render: function (option) {
+            return new BeFileUpload(option);
+        }
     });
 });
--- plugins/BeImgUpload.js
+++ plugins/BeImgUpload.js
@@ -3,135 +3,127 @@ layui.define(['jquery', 'layer', 'form'], function (exports) {
     let $ = layui.jquery;
     let layer = layui.layer;
 
-    let render = function (option) {
+    function BeImgUpload(option) {
         if (!option.readonly) {
             option.readonly = false;
         }
 
-        option.value = option.value ? option.value : [];
-        $(option.id).append(`<input type="hidden" class="upload-hidden-input" name="${option.name}"/>`)
-        addImg(option);
+        this.option = option;
+        this.option.value = this.option.value ? this.option.value : [];
+        $(this.option.id).append(`<input type="hidden" class="upload-hidden-input" name="${this.option.name}"/>`);
+        this.addImg();
 
-        if (option.readonly == false) {
-            $(option.id).append(`
-            <div class="upload-wapper">
-                <i class="upload-icon"></i>
-                <input type="file" class="upload-input" accept="${option.type ? option.type : 'image/*'}">
-            </div>`);
-            if (option.value.length >= option.num) {
-                $(option.id).find(".upload-wapper").hide();
-            }
+        if (!this.option.readonly && this.option.value.length < this.option.num) {
+            $(this.option.id).append(`
+                <div class="upload-wapper">
+                    <i class="upload-icon"></i>
+                    <input type="file" class="upload-input" accept="${this.option.type ? this.option.type : 'image/*'}">
+                </div>`);
         }
-        appendChange(option);
-        clickEvent(option);
+
+        this.appendChange();
+        this.clickEvent();
     }
 
-    let appendChange = function (option) {
-        $(option.id + " input[type=file]").on('change', function (event) {
-            // 获取选择的文件列表
+    BeImgUpload.prototype.appendChange = function () {
+        let that = this;
+        $(this.option.id + " input[type=file]").on('change', function (event) {
             let files = event.target.files;
             let file = files[0];
-            // 可以将文件对象传递给其他函数进行进一步处理
-            processFile(option, file);
-            $(option.id + " input[type=file]").val(""); // 强制清除输入框的值
+            that.processFile(file);
+            $(that.option.id + " input[type=file]").val("");
         });
-        // 清除已选中的文件
         $(this).val('');
     }
 
-
-    // 在此处执行对文件的自定义处理逻辑
-    let processFile = function (option, file) {
+    BeImgUpload.prototype.processFile = function (file) {
+        let that = this;
         if (file) {
-            if (option.size && option.size <= file.size / 1024 / 1024) {
-                layer.msg("上传文件大小不得超过" + option.size + "M", {icon: 0});
+            if (that.option.size && that.option.size <= file.size / 1024 / 1024) {
+                layer.msg("上传文件大小不得超过" + that.option.size + "M", {icon: 0});
             } else {
-                let that = $(this);
                 let formData = new FormData();
                 formData.append("file", file);
                 $.ajax({
                     url: ctx_ + "document/file/upload",
                     data: formData,
                     type: "post",
-                    processData: false,  // 禁止对数据进行序列化
-                    contentType: false,  // 不设置请求头的 Content-Type
+                    processData: false,
+                    contentType: false,
                     success: function (result) {
                         $(`<div class="image-section" data-id="${result.id}">
                                 <img class="image-show" src="/document/file/download?fileId=${result.id}">
                                 <div class="image-shade"></div>
-                               ${option.readonly ? '' : '<i class="delete-icon"></i>'}
+                               ${that.option.readonly ? '' : '<i class="delete-icon"></i>'}
                                 <i class="zoom-icon"></i>
-                            </div>`).insertBefore(option.id + ' .upload-wapper');
-                        option.value.push(result.id);
-                        $(option.id + " input[name='" + option.name + "']").val(option.value.join(','))
-                        showAddBtn(option);
+                            </div>`).insertBefore(that.option.id + ' .upload-wapper');
+                        that.option.value.push(result.id);
+                        $(that.option.id + " input[name='" + that.option.name + "']").val(that.option.value.join(','));
+                        that.showAddBtn();
                     },
                     error: function (xhr, status, error) {
-                        layer.msg("上传失败", {icon: 2})
+                        layer.msg("上传失败", {icon: 2});
                     }
                 });
             }
         }
     }
 
-    //添加预览图片
-    let addImg = function (option) {
-        if (option.value) {
-            for (let i = 0; i < option.value.length; i++) {
-                $(option.id).append(`
-                <div class="image-section" data-id="${option.value[i]}">
-                    <img class="image-show" src="/document/file/download?fileId=${option.value[i]}">
+    BeImgUpload.prototype.addImg = function () {
+        let that = this;
+        if (this.option.value) {
+            for (let i = 0; i < this.option.value.length; ++i) {
+                $(this.option.id).append(`
+                <div class="image-section" data-id="${this.option.value[i]}">
+                    <img class="image-show" src="/document/file/download?fileId=${this.option.value[i]}">
                     <div class="image-shade"></div>
-                   ${option.readonly ? '' : '<i class="delete-icon"></i>'}
+                   ${this.option.readonly ? '' : '<i class="delete-icon"></i>'}
                     <i class="zoom-icon"></i>
                 </div>`);
             }
-            $(option.id + " input[name='" + option.name + "']").val(option.value.join(','))
+            $(this.option.id + " input[name='" + this.option.name + "']").val(this.option.value.join(','));
         }
     }
 
-
-    //删除或者放大图片
-    function clickEvent(option) {
-        // 在此处执行点击删除图标后的逻辑
-        $(option.id).on('click', '.delete-icon', function () {
-            let that = $(this);
-            let id = that.parent(".image-section").data("id");
-
-            that.parent(".image-section").remove();
-            showAddBtn(option);
-            let index = option.value.indexOf(id);
+    BeImgUpload.prototype.clickEvent = function () {
+        let that = this;
+        $(this.option.id).on('click', '.delete-icon', function () {
+            let id = $(this).parent(".image-section").data("id");
+            $(this).parent(".image-section").remove();
+            let index = that.option.value.indexOf(id);
             if (index !== -1) {
-                option.value.splice(index, 1);
+                that.option.value.splice(index, 1);
             }
-            $(option.id + " input[name='" + option.name + "']").val(option.value.join(','));
+            $(that.option.id + " input[name='" + that.option.name + "']").val(that.option.value.join(','));
+            that.showAddBtn();
         });
 
-        // 在此处执行点击放大图标后的逻辑
-        $(option.id).on('click', '.zoom-icon', function () {
+        $(this.option.id).on('click', '.zoom-icon', function () {
             let img = $(this).parent(".image-section").children("img")[0];
             layer.open({
                 type: 1,
-                area: ["auto", "auto"], // 宽高
-                title: false, // 不显示标题栏
+                area: ["auto", "auto"],
+                title: false,
                 closeBtn: 1,
-                shadeClose: true, // 点击遮罩关闭层
+                shadeClose: true,
                 content: "<img src='" + img.src + "' style='width: auto !important;max-height: 600px !important'/>"
             });
         });
     }
 
-    //显示或隐藏添加按钮
-    function showAddBtn(option) {
-        let count = $(option.id + ' .image-section').length;
-        if (option.num && count < option.num) {
-            $(option.id).find(".upload-wapper").show();
+    BeImgUpload.prototype.showAddBtn = function () {
+        let count = $(this.option.id + ' .image-section').length;
+        if (this.option.num && count < this.option.num) {
+            $(this.option.id).find(".upload-wapper").show();
         } else {
-            $(option.id).find(".upload-wapper").hide();
+            $(this.option.id).find(".upload-wapper").hide();
         }
     }
 
     exports('BeImgUpload', {
-        render
+        render: function (option) {
+            return new BeImgUpload(option);
+        }
     });
 });
+
