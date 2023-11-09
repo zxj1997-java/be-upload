ddda7266b7833024efa6739a685a092b7a616b9c
zhangxingju <1533195362@qq.com>
2023-11-09 10:26:38 +0800

    style(图片上传):bug修复2

--- layui_exts/be-upload/common/common.js
+++ layui_exts/be-upload/common/common.js
@@ -9,7 +9,7 @@ let global = {
 
 // 假设 firstMod 模块文件所在路径在： /js/layui_exts/firstMod.js
 layui.config({
-    base: '../layui_exts/be-upload/' // 配置 Layui 第三方扩展模块存放的基础目录
+    base: './layui_exts/be-upload/' // 配置 Layui 第三方扩展模块存放的基础目录
 }).extend({
     BeImgUpload: 'BeImgUpload', // 定义模块名和模块文件路径，继承 layui.config 的 base 路径
     BeFileUpload: 'BeFileUpload',
